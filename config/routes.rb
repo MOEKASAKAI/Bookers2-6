21e6bb6e9c7e7731048a8fbfeb7fd5413c2a016a
MOEKASAKAI <wpi.floves2@gmail.com>
2021-02-03 08:04:09 +0000

    [Add] 4-10 プロフィール編集ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/edit' # 削除/書き換えが必要
   devise_for :users, controllers: {
     sessions: 'devise/sessions',
   registrations: 'devise/registrations'
@@ -6,4 +7,5 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
+  resources :users, only: [:show, :edit, :update]
 end
