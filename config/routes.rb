e83ad9828b1d976cfea4711c2b4db10b8ad068d7
MOEKASAKAI <wpi.floves2@gmail.com>
2021-02-04 07:34:49 +0000

    [Add] 4-10 ユーザ一覧機能

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
+  resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] 
-  resources :users, only: [:show, :edit, :update]
 end
