4ba8ca34a56f072ef757a95e093cde6f69713f88
MOEKASAKAI <wpi.floves2@gmail.com>
2021-02-07 09:14:05 +0000

    [Finish] 4-10

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '344d2061c74fb540a714605ecc030f96992532e30ededa16c12c116dc4e4d8646c19fd677b94d4d2740b4b40905ac0e8ee275061343675dc6ee1a1f039306ec6'
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'devise/sessions',
-  registrations: 'devise/registrations'
-  }
+  devise_for :users
   # ログインと新規登録の時の指定
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
+  get 'home/about' => "homes#about"
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] 
 end
